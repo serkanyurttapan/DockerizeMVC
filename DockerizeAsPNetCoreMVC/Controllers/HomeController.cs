5c833032e56fcd3f9f01afe8fb3db82cc20978a1
serkanyurttapan_ <serkan.yurttapan@asistbt.com.tr>
2021-01-09 19:04:59 +0300

    docker send messagge

--- DockerizeAsPNetCoreMVC/Controllers/HomeController.cs
+++ DockerizeAsPNetCoreMVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using DockerizeAsPNetCoreMVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,10 @@ namespace DockerizeAsPNetCoreMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IFileProvider _fileProvider;
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
         {
+            _fileProvider = fileProvider;
             _logger = logger;
         }
 
@@ -29,18 +31,20 @@ namespace DockerizeAsPNetCoreMVC.Controllers
         {
             return View();
         }
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").Select(x => x.Name).ToList();
+            return View(images);
+        }
         [HttpPost]
         public async Task<IActionResult> ImageSave(IFormFile formFile)
         {
             if (formFile != null && formFile.Length > 0)
             {
-
-                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName);
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
-                using (var stream = new FileStream(path, FileMode.Create))
-                {
-                    await formFile.CopyToAsync(stream);
-                }
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName);
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+                using FileStream stream = new FileStream(path, FileMode.Create);
+                await formFile.CopyToAsync(stream);
             }
             return View();
         }
